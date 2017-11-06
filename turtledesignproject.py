7dfcf1bfb9788bca3d009e21f0b119ed8da6cfd2
rasprilla <33330256+rasprilla@users.noreply.github.com>
2017-11-06 15:19:47 -0500

    Add files via upload

---
+++ turtledesignproject.py
@@ -0,0 +1,301 @@
+#myDesign
+import turtle
+from myfunctionfile import*
+bob = turtle.Turtle()
+bob.width(5)
+t = bob
+
+turtle.bgcolor("black")
+bob.speed(11)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+        
+jump(t,-400,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+        
+jump(t,-300,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-200,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-100,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+        
+jump(t,100,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+
+jump(t,200,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+
+jump(t,300,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,400,0)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("red")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,400,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,300,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+
+jump(t,200,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+
+jump(t,100,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,0,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+
+jump(t,-100,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+ 
+jump(t,-200,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-300,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-400,100)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("blue")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-400,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-300,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-200,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-100,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,0,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+        
+jump(t,100,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,200,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,300,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,400,200)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,400,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,300,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,200,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,100,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,0,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+        
+jump(t,-100,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-200,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-300,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
+jump(t,-400,300)
+
+for times in range(1):
+        t.begin_fill()
+        t.color("yellow")
+        polygon(t,4,100)
+        t.end_fill()
+
